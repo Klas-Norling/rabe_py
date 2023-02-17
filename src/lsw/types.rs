010841142239c00b6a706b7d2fb08d20134d13c1
Klas-Norling <klasaman99@hotmail.com>
2023-02-17 14:00:24 +0100

    Fixed requested changes in PR

--- src/lsw/types.rs
+++ src/lsw/types.rs
@@ -1,10 +1,4 @@
 //! LSW implementation of structs and types
-//! ---
-//!
-//! This is based on the  [original paper](https://some webage) and is implented in [the aw11 source](rabe::schemes::aw11)
-//!
-//!
-//! [`]
 
 
 use rabe::schemes::lsw::{
@@ -19,28 +13,28 @@ use pyo3::exceptions::PyValueError;
 
 use crate::serializable;
 
-// Wrapper for LSW public key
+/// Wrapper for LSW public key
 #[pyclass]
 #[derive(serde::Serialize,serde::Deserialize)]
 pub struct PyKpAbePublicKey {
     pub pk: KpAbePublicKey,
 }
 
-// Wrapper for LSW master key
+/// Wrapper for LSW master key
 #[pyclass]
 #[derive(serde::Serialize,serde::Deserialize)]
 pub struct PyKpAbeMasterKey {
     pub msk: KpAbeMasterKey,
 }
 
-// Wrapper for LSW secret key
+/// Wrapper for LSW secret key
 #[pyclass]
 #[derive(serde::Serialize,serde::Deserialize)]
 pub struct PyKpAbeSecretKey {
     pub sk: KpAbeSecretKey,
 }
 
-// Wrapper for LSW ciphertext
+/// Wrapper for LSW ciphertext
 #[pyclass]
 #[derive(serde::Serialize,serde::Deserialize)]
 pub struct PyKpAbeCiphertext {
